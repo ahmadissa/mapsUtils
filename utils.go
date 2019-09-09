73334efa310dc26634cc67f550cf81eb7fe79f32
ahmadissa <issa.ahmd@gmail.com>
2019-09-09 10:52:46 +1000

    Create utils.go

---
+++ utils.go
@@ -0,0 +1,64 @@
+package Utils
+
+import (
+	"strconv"
+	"strings"
+)
+
+func GetString(obj map[string]interface{}, key string) (string, bool) {
+	switch obj[key].(type) {
+	case string:
+		return obj[key].(string), true
+	}
+	return "", false
+}
+
+func GetBool(obj map[string]interface{}, key string) (bool, bool) {
+	switch obj[key].(type) {
+	case bool:
+		return obj[key].(bool), true
+	}
+	return false, false
+}
+
+//StrToInt
+func StrToInt(v string) (int, error) {
+	v = strings.TrimSpace(v)
+	res, e := strconv.Atoi(v)
+	if e != nil {
+		return 0, e
+	}
+	return res, nil
+}
+
+//StrToInt64
+func StrToInt64(v string) (int64, error) {
+	v = strings.TrimSpace(v)
+	res, e := strconv.ParseInt(v, 10, 64)
+	if e != nil {
+		return 0, e
+	}
+	return res, nil
+}
+
+func GetInt(obj map[string]interface{}, key string) (int, bool) {
+	switch obj[key].(type) {
+	case int:
+		return obj[key].(int), true
+	}
+	return 0, false
+}
+func GetFloat64(obj map[string]interface{}, key string) (float64, bool) {
+	switch obj[key].(type) {
+	case float64:
+		return obj[key].(float64), true
+	}
+	return 0, false
+}
+func GetInt64(obj map[string]interface{}, key string) (int64, bool) {
+	switch obj[key].(type) {
+	case int64:
+		return obj[key].(int64), true
+	}
+	return 0, false
+}
